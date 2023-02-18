6f00e63f5e5e85df895d7c96273b7f53ca1abc6e
JoelFredriksson <112383070+JoelFredriksson@users.noreply.github.com>
2023-02-18 14:19:45 +0100

    first commit

---
+++ vue-project/src/components/NavBar.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template>
+  <RouterLink to="/"></RouterLink>
+  <RouterLink to="about">About</RouterLink>
+  <RouterView />
+</template>
---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2>About</h2>
+</template>
+
+<style></style>
