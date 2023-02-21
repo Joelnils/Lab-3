017e8a5f85973f2fd4f2ecb54e102c553cc1ca02
JoelFredriksson <joel.fredriksson91@hotmail.se>
2023-02-21 10:44:21 +0100

    Heroimage

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,31 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div
+    class="hero-image"
+    :style="{ 'background-image': 'url(' + imageUrl + ')' }"
+  ></div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: "",
+    };
+  },
+  mounted() {
+    this.imageUrl =
+      "https://images.unsplash.com/photo-1596838132731-3301c3fd4317?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80c";
+  },
+};
+</script>
+
+<style scoped>
+.hero-image {
+  width: 100%;
+  height: 100vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+</style>
