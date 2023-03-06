d0379d4a6bbc945f012400e0f5273af39b0e9c9b
JoRuPeRu <johan.rutberg@iths.se>
2023-03-06 23:17:43 +0100

    Diverse fix med länkningar samt justering av wheel till mindre storlek ;-)

--- vue-project/src/views/WheelOfFortune.vue
+++ vue-project/src/views/WheelOfFortune.vue
@@ -11,8 +11,14 @@
 
 <style scoped>
 .extra{
-    height: 100vh;
-    margin-top: 80px;
-    background-color: #02070c;
+    background: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.5) 0%,
+      rgba(0, 0, 0, 0.8) 100%
+    ),
+    url("https://images.unsplash.com/photo-1596838132731-3301c3fd4317?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+      no-repeat center fixed;
+  background-size: cover;
+  height: 110vh;
 }
 </style>
